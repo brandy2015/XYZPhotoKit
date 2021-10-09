96d0bb7c36e1024d84c2dccccf386f75fa67c6be
XYZ <10484324+brandy2015@users.noreply.github.com>
2021-10-09 13:37:11 +0800

    version 3.0.4

--- XYZPhotoKit/Source/XYZPhoto_XYZPicExif_Exif.swift
+++ XYZPhotoKit/Source/XYZPhoto_XYZPicExif_Exif.swift
@@ -148,3 +148,27 @@ extension Exif: CustomStringConvertible {
         return description
     }
 }
+
+import Foundation
+
+private extension String {
+    func stringArray() -> [String]? {
+        guard self.count > 2 else {
+            return nil
+        }
+        
+        let parenthesesDeletedText = String(self.prefix(self.count - 1)).suffix(self.count - 2)
+        let separated = parenthesesDeletedText.components(separatedBy: ",")
+        return separated.map{ $0.trimmingCharacters(in: .whitespaces) }
+    }
+    
+    func isNotHyphen() -> String? {
+        return self == "-" ? nil : self
+    }
+    
+    func dateFromString() -> Date? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy:MM:dd HH:mm:ss"
+        return formatter.date(from: self)
+    }
+}
